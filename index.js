7abb33dd064216a4ce263c8953b0f116ab6abc15
Erel Kapah <erel.kapah@gmail.com>
2015-06-28 11:55:56 +0300

    Support @import #1

--- index.js
+++ index.js
@@ -18,48 +18,70 @@ StylusFilter.prototype.constructor = StylusFilter;
 StylusFilter.prototype.extensions = ['styl'];
 StylusFilter.prototype.targetExtension = 'css';
 
-StylusFilter.prototype.processString = function (str) {
+StylusFilter.prototype.processString = function(str) {
 	var opts = this.options;
+
+	var importRegex = new RegExp(/@import ['"].*['"]/);
+	while (importRegex.test(str)) {
+		var imp = importRegex.exec(str);
+		var sourceReg = new RegExp(/['"].*['"]/);
+		var extReg = new RegExp(/.styl/);
+		var src = sourceReg.exec(imp[0])[0].replace(/"/g, '').replace(/'/g, '');
+		if (!extReg.test(src)) {
+			src = src + '.styl';
+		}
+
+		src = this.inputTree.inputTree.inputTree + '/' + this.inputTree.srcDir + '/' + src;
+
+		if (!opts.import) {
+			opts.import = [];
+		}
+
+		opts.import.push(src);
+
+		str = str.replace(importRegex, '');
+	}
+
 	var s = stylus(str);
 
 	if (opts.include) {
-		opts.include.forEach(function (el) {
+		opts.include.forEach(function(el) {
 			s.include(el);
 		});
 	}
 
 	if (opts.use) {
-		opts.use.forEach(function (el) {
+		opts.use.forEach(function(el) {
 			s.use(el());
 		});
 	}
 
 	if (opts.import) {
-		opts.import.forEach(function (el) {
+		opts.import.forEach(function(el) {
 			s.import(el);
 		});
 	}
 
 	if (opts.urlFn) {
-		opts.urlFn.forEach(function (el) {
+		opts.urlFn.forEach(function(el) {
 			s.define(el, stylus.url());
 		});
 	}
 
 	if (opts.set) {
-		Object.keys(opts.set).forEach(function (key) {
+		Object.keys(opts.set).forEach(function(key) {
 			s.set(key, opts.set[key]);
 		});
 	}
 
 	if (opts.define) {
-		Object.keys(opts.define).forEach(function (key) {
+		Object.keys(opts.define).forEach(function(key) {
 			s.define(key, opts.define[key]);
 		});
 	}
 
-	return new RSVP.Promise(function (resolve, reject) {
-		s.render(function (err, data) {
+	return new RSVP.Promise(function(resolve, reject) {
+		s.render(function(err, data) {
 			if (err) {
 				return reject(err);
 			}
