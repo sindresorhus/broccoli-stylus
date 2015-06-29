44a0555d8ebbbbd39448a11bdfbc61350607996d
Erel Kapah <erel.kapah@gmail.com>
2015-06-29 18:31:28 +0300

    add some comments to import fix

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ StylusFilter.prototype.constructor = StylusFilter;
 StylusFilter.prototype.extensions = ['styl'];
 StylusFilter.prototype.targetExtension = 'css';
 
-StylusFilter.prototype.processString = function(str) {
+StylusFilter.prototype.processString = function (str) {
 	var opts = this.options;
 
 	var importRegex = new RegExp(/@import ['"].*['"]/);
@@ -26,62 +26,70 @@ StylusFilter.prototype.processString = function(str) {
 		var imp = importRegex.exec(str);
 		var sourceReg = new RegExp(/['"].*['"]/);
 		var extReg = new RegExp(/.styl/);
+
+		// get import file src and remove quotes
 		var src = sourceReg.exec(imp[0])[0].replace(/"/g, '').replace(/'/g, '');
+
+		// add '.styl' to filename if not exist
 		if (!extReg.test(src)) {
 			src = src + '.styl';
 		}
 
+		// building file path to add to import array
 		src = this.inputTree.inputTree.inputTree + '/' + this.inputTree.srcDir + '/' + src;
 
+		// create import array if not exist
 		if (!opts.import) {
 			opts.import = [];
 		}
 
+		// push file source to import array
 		opts.import.push(src);
 
+		// remove the import from stylus src
 		str = str.replace(importRegex, '');
 	}
 
 	var s = stylus(str);
 
 	if (opts.include) {
-		opts.include.forEach(function(el) {
+		opts.include.forEach(function (el) {
 			s.include(el);
 		});
 	}
 
 	if (opts.use) {
-		opts.use.forEach(function(el) {
+		opts.use.forEach(function (el) {
 			s.use(el());
 		});
 	}
 
 	if (opts.import) {
-		opts.import.forEach(function(el) {
+		opts.import.forEach(function (el) {
 			s.import(el);
 		});
 	}
 
 	if (opts.urlFn) {
-		opts.urlFn.forEach(function(el) {
+		opts.urlFn.forEach(function (el) {
 			s.define(el, stylus.url());
 		});
 	}
 
 	if (opts.set) {
-		Object.keys(opts.set).forEach(function(key) {
+		Object.keys(opts.set).forEach(function (key) {
 			s.set(key, opts.set[key]);
 		});
 	}
 
 	if (opts.define) {
-		Object.keys(opts.define).forEach(function(key) {
+		Object.keys(opts.define).forEach(function (key) {
 			s.define(key, opts.define[key]);
 		});
 	}
 
-	return new RSVP.Promise(function(resolve, reject) {
-		s.render(function(err, data) {
+	return new RSVP.Promise(function (resolve, reject) {
+		s.render(function (err, data) {
 			if (err) {
 				return reject(err);
 			}
